b052f247e40f888700e3037dbe72e46007210df5
Uyen Nguyen <xuyennguyenx@LearniOSFreecoms-Mac.local>
2017-06-22 13:59:56 -0400

    don't pull from here

---
+++ UniversalDomination/UniversalDomination/Territory.swift
@@ -0,0 +1,23 @@
+//
+//  Territory.swift
+//  UniversalDomination
+//
+//  Created by Uyen Nguyen on 6/22/17.
+//  Copyright © 2017 Group1. All rights reserved.
+//
+
+import Foundation
+
+class Territory {
+    var id : Double
+    var x: Double
+    var y: Double
+    
+    var name: String?
+    var player: String?
+    
+    var armies: Double
+    var planets: Double
+    
+    
+}
