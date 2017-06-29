d6a05c8b5d4600491168f684d7e6f3ff03be19a3
Sandy Demian <sdemian@knights.ucf.edu>
2017-06-29 15:05:42 -0400

    added action to planet buttons (troops are added when a planet is clicked), added some methods in Planet class, added Player class

--- UniversalDomination/UniversalDomination/GameViewController.swift
+++ UniversalDomination/UniversalDomination/GameViewController.swift
@@ -10,53 +10,73 @@ import UIKit
 import SpriteKit
 import GameplayKit
 
-class GameViewController: UIViewController {
-    
-    
+ class GameViewController: UIViewController
+ {
+    let numberOfPlanets = 10
+    var planets = [Planet]()
+    @IBOutlet var PlanetsButtons: [UIButton]!
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
+        
         super.viewDidLoad()
         
         let scene = GameScene(size: view.bounds.size)
         
+        
         // configure the view
         let skView = view as! SKView
-        
-
         skView.isMultipleTouchEnabled = false
         skView.showsFPS = true
         skView.showsNodeCount = true
         skView.ignoresSiblingOrder = true
         scene.scaleMode = .resizeFill
-        
-        // create and configure the scene
         scene.scaleMode = .aspectFill
         
-        
-        // present the scene
         skView.presentScene(scene)
- 
+        
+        
+        for index in 1...numberOfPlanets
+        {
+            // add the planets and the button outlets
+            planets.append(Planet(bttn: PlanetsButtons[index-1]))
+        }
 
 
     }
-    
 
-    override var shouldAutorotate: Bool {
+    
+    @IBAction func planetClicked(_ sender: Any)
+    {
+        guard let button = sender as? UIButton else
+        {
+            return
+        }
+        
+        planets[button.tag].addTroops(value: 1)
+        button.setTitle(String(planets[button.tag].getTroops()), for: UIControlState.normal)
+    }
+    
+    
+    override var shouldAutorotate: Bool
+    {
         return true
     }
-    
 
+    
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask
     {
         return .landscapeLeft
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
 
-    override var prefersStatusBarHidden: Bool {
+    override var prefersStatusBarHidden: Bool
+    {
         return true
     }
 }
