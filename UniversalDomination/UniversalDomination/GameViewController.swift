85a1cee8c7afd272cff694c7bad2077a3e8fbaba
Uyen Nguyen <xuyennguyenx@LearniOSFreecoms-Mac.local>
2017-06-29 13:28:40 -0400

    countDownTimer in GameControllerView

--- UniversalDomination/UniversalDomination/GameViewController.swift
+++ UniversalDomination/UniversalDomination/GameViewController.swift
@@ -11,9 +11,7 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
-    
-    
-
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,7 +20,7 @@ class GameViewController: UIViewController {
         // configure the view
         let skView = view as! SKView
         
-
+        
         skView.isMultipleTouchEnabled = false
         skView.showsFPS = true
         skView.showsNodeCount = true
@@ -35,28 +33,72 @@ class GameViewController: UIViewController {
         
         // present the scene
         skView.presentScene(scene)
- 
-
-
+        
+        
+        
     }
-    
-
     override var shouldAutorotate: Bool {
         return true
     }
     
-
+    
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask
     {
         return .landscapeLeft
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
-
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
+    
+    
+    @IBOutlet weak internal var countDownTimer: UILabel!
+    
+    var seconds = 60
+    var timer = Timer()
+    var timerIsOn = false
+    
+    @IBAction func startButton(_ sender: Any) {
+        if timerIsOn == false {
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(GameViewController.updateTimer)), userInfo: nil, repeats: true)
+            timerIsOn = true
+        }
+    }
+   
+    @IBAction func endButton(_ sender: Any) {
+        timer.invalidate()
+        seconds = 0
+        countDownTimer.text = "\(seconds)"
+        timerIsOn = false
+    }
+
+    func updateTimer() {
+        seconds -= 1
+        countDownTimer.text = "\(seconds)"
+    
+
+    
+}
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
